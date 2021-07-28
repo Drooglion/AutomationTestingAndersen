ae1ebb7bf89be8fde971aaebb05bd1a8b818c142
Drooglion <starsik.21s@gmail.clm>
2021-07-29 00:24:06 +0300

    Done with remarks

--- src/main/java/H2/OOP.java
+++ src/main/java/H2/OOP.java
@@ -6,15 +6,23 @@ public class OOP {
 //    3. Создать класс дог и переопределить  в нем основые методы
 //    4. Перегрузить метод рост вес и движение с разыми параметрами
 //       (футы,метры,метрические еденицы ...)
+}
+interface Animal {
 
+    double getWeight();
+    double getHeight();
 
+    void setWeight(double w);
+    void setHeight(double h);
+
+    void lunchTime();
 }
 
-class Animal {
-    private double weight;
-    private double height;
-    private boolean hunger;
-    private int speed;
+class Sobaka implements Animal {
+    double weight = 0;
+    double height = 0;
+    boolean hunger = false;
+    int speed = 0;
 
     public double getWeight() {
         return weight;
@@ -24,12 +32,14 @@ class Animal {
         return height;
     }
 
+
     public void setWeight(double w) {
         weight = w;
     }
 
     public void setHeight(double h) {
         height = h;
+
     }
 
     public void lunchTime() {
@@ -37,26 +47,42 @@ class Animal {
     }
 }
 
-class Sobaka extends Animal {
-}
-
-class Dog extends Sobaka {
+class Dog  implements Animal {
 
     private double weight;
     private double height;
 
-
-    public void setWeight(double w) {
+    public double getWeight() {
+        if (weight != 0) {
+            return weight;
+        } else {
+            return -1;
+        }
+    }
+    public double getHeight() {
+        if (height != 0) {
+            return height;
+        } else {
+            return -1;
+        }
+    }
+    public void setWeight(double weightFunts) {
         double funts = 2.2046;
-        double converting = w * 2.2046;
+        double converting = weightFunts * 2.2046;
         weight = converting;
+    }
 
+    public void setWeight(int kilograms) {
+    weight = kilograms;
     }
 
     public void setHeight(double h) {
         double ft = 3.2808;
         double heightInFt = h * ft;
         height = heightInFt;
+
+    }
+    public void lunchTime() {
 
     }
 
